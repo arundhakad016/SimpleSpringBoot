fbfe612f12ab1ea8b3520673f7779f1ecefe4ce1
arundhakad <arundhakad016@gmail.com>
2021-04-14 12:19:55 +0530

    first commit simple spring boot

---
+++ src/test/java/com/springboot/arun/SimplespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.arun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
